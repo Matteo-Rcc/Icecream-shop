c22024c73b54d2336fb015fd5f271ea9ed259eae
Racca <raccanello.matteo@gmail.com>
2020-12-08 22:40:22 +0100

    Close #4

---
+++ src/test/java/it/unipd/tos/model/MenuItemTest.java
@@ -0,0 +1,24 @@
+package it.unipd.tos.model;
+
+import static org.junit.Assert.*;
+import it.unipd.tos.model.MenuItem;
+
+import org.junit.Test;
+
+public class MenuItemTest {
+
+    @Test
+    public void MenuItem_costructor_test() {
+        /* DICHIARAZIONI */
+        itemType tipo = itemType.Gelati;
+        String nome = "g1";
+        double costo = 1.5;
+        MenuItem prod = new MenuItem(tipo, nome, costo);
+        
+        /* ASSERT */
+        assertEquals(tipo, prod.getType());
+        assertEquals(nome, prod.getName());
+        assertEquals(costo, prod.getPrice(), 0.0001);
+    }
+
+}
---
+++ src/test/java/it/unipd/tos/model/UserTest.java
@@ -0,0 +1,23 @@
+package it.unipd.tos.model;
+
+import static org.junit.Assert.*;
+import java.time.LocalDate;
+import org.junit.Test;
+
+import it.unipd.tos.model.User;
+
+public class UserTest {
+    @Test
+    public void user_costructor_test() {
+        /* DIHIARAZIONI */
+        int id = 1;
+        String nome = "u1";
+        LocalDate n = LocalDate.of(1999, 5, 3);
+        User u1 = new User(id, nome, n);
+        
+        /* ASSERT */
+        assertEquals(id, u1.getId());
+        assertEquals(nome, u1.getName());
+        assertEquals(n, u1.getDate());
+    }
+}
